037eeb513dd5ac309770588e5e882e72eff2957d
lukDev <lukas.mautner98@gmail.com>
2024-09-10 08:59:10 +0200

    more classes exposed

--- conceptnet_local/__init__.py
+++ conceptnet_local/__init__.py
@@ -1,4 +1,4 @@
 from ._cn_service import get_all_edges, get_relatedness, setup_sqlite_db, close_sqlite_db, Relation
-from ._a_star import AStar, Path, format_path, NoPathFoundError, SearchRelation
+from ._a_star import AStar, Path, format_path, NoPathFoundError, SearchRelation, Concept
 from ._a_star_variants import get_a_star_variant, CostFunction, HeuristicFunction
 from ._yen_greedy import get_offshoot_paths, PathWithHash
