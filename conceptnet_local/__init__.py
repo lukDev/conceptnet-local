1827d0a03b2a48343e866344a160ed36b6a25942
lukDev <lukas.mautner98@gmail.com>
2024-10-12 17:20:01 +0200

    dotenv integrated

--- conceptnet_local/__init__.py
+++ conceptnet_local/__init__.py
@@ -1,3 +1,5 @@
+import _setup
+
 from ._cn_service import get_all_edges, get_relatedness, get_all_concept_ids, setup_sqlite_db, close_sqlite_db, Relation
 from ._a_star import AStar, Path, format_path, NoPathFoundError, SearchRelation, Concept
 from ._a_star_variants import get_a_star_variant, CostFunction, HeuristicFunction
