06a39932428a29b6c665cd0fca89f86b263fa408
lukDev <lukas.mautner98@gmail.com>
2025-01-18 16:44:45 +0100

    typo

--- conceptnet_local/__init__.py
+++ conceptnet_local/__init__.py
@@ -15,6 +15,6 @@ from ._cn_service import (
 )
 from ._a_star import AStar, Path, NoPathFoundError, SearchRelation, Concept
 from ._a_star_variants import get_a_star_variant, CostFunction, HeuristicFunction
-from _concept_extraction import get_concepts_in_text, ConceptInText
+from ._concept_extraction import get_concepts_in_text, ConceptInText
 from ._utils import format_path, get_formatted_link_label, get_natural_concept_from_id, get_id_from_natural_concept, sanitize_term
 from ._yen_greedy import get_offshoot_paths
